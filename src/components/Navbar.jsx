7572e10d940badc9472a2071e195b0b4a417a414
yassirab02 <yassirabourial86@gmail.com>
2024-07-16 14:44:38 +0100

    adding some changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
           <ul className="hidden lg:flex ml-14 space-x-12">
             {navItems.map((item, index) => (
               <li key={index}>
-                <a href={item.href}>{item.label}</a>
+                <a href={item.href} className="hover:text-orange-600">{item.label}</a>
               </li>
             ))}
           </ul>
